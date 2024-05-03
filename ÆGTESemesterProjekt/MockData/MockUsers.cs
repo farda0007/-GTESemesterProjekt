cb3a677d0c272e6673cba36916b2ebc552659c1f
Lucasloa <150661710+Lucasloa@users.noreply.github.com>
2024-05-03 13:33:26 +0200

    MockUsers

---
+++ ÆGTESemesterProjekt/MockData/MockUsers.cs
@@ -0,0 +1,19 @@
+﻿using ÆGTESemesterProjekt.Models;
+
+namespace ÆGTESemesterProjekt.MockData
+{
+    public class MockUsers
+    {
+        private static List<Employee> users = new List<Employee>()
+        {
+            new Employee(1, "Arda", "Arda", "123", 60714904, "Arda@mail.com"),
+            new Employee(1, "Akhmed", "Akh", "Admin", 13131919, "Akhmedes@mail.com")
+        };
+
+        public static List<Employee> GetUsers()
+        {
+            return users;
+        }
+
+    }
+}
