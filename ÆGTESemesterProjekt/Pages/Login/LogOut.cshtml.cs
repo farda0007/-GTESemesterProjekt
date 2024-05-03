898bf61d9bc6a75be38b197ba21e2986fe633321
Lucasloa <150661710+Lucasloa@users.noreply.github.com>
2024-05-03 13:13:40 +0200

    Log user user tiiajdaid

---
+++ ÆGTESemesterProjekt/Pages/Login/LogOut.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ÆGTESemesterProjekt.Pages.Login
+{
+    public class LogOutModel : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            //LogInPageModel.LoggedInUser = null;
+
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/index");
+        }
+    }
+}
---
+++ ÆGTESemesterProjekt/Pages/User/CreateUser.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace ÆGTESemesterProjekt.Pages.User
+{
+    [Authorize(Roles = "admin")]
+    public class CreateUserModel : PageModel
+    {
+        private PasswordHasher<string> passwordHasher;
+        private UserService _userService;
+        [BindProperty]
+        public string UserName { get; set; }
+        [BindProperty, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public CreateUserModel(UserService userService)
+        {
+            this.passwordHasher = new PasswordHasher<string>();
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            await _userService.AddUserAsync(new User(UserName, passwordHasher.HashPassword(null, Password)));
+            //_userService.AddUser(new User(UserName, Password));
+            return RedirectToPage("/Item/GetAllItems");
+        }
+    }
+}
