cd6ce89950c637aa199d21662578a17f80d95964
farda0007 <ardaumutyildiz@gmail.com>
2024-05-07 12:25:05 +0200

    sync og commit

--- ÆGTESemesterProjekt/ProductTypeModels/Headset.cs
+++ ÆGTESemesterProjekt/ProductTypeModels/Headset.cs
@@ -2,20 +2,20 @@
 
 namespace ÆGTESemesterProjekt.ProductTypeModels
 {
-    public class Headset : Product
-    {
-        public string Brand { get; set; }
-        public bool Wireless { get; set; }
-        public List<Headset> Headsets { get; set; }
-        public Headset()
-        {
-        }
+    //public class Headset : Product
+    //{
+    //    public string Brand { get; set; }
+    //    public bool Wireless { get; set; }
+    //    public List<Headset> Headsets { get; set; }
+    //    public Headset()
+    //    {
+    //    }
 
-        public Headset(string brand, bool wireless, List<Headset> headsets, int id, string productName, int price) : base(id, productName, price)
-        {
-            Brand = brand;
-            Wireless = wireless;
-            Headsets = headsets;
-        }
-    }
+    //    public Headset(string brand, bool wireless, List<Headset> headsets, int id, string productName, int price) : base(id, productName, price)
+    //    {
+    //        Brand = brand;
+    //        Wireless = wireless;
+    //        Headsets = headsets;
+    //    }
+    //}
 }
