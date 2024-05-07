2a313376075179511bbae8d3e97509b50b133eab
Lucasloa <150661710+Lucasloa@users.noreply.github.com>
2024-05-07 13:33:56 +0200

    save json virker ikke

--- ÆGTESemesterProjekt/Services/ProductService.cs
+++ ÆGTESemesterProjekt/Services/ProductService.cs
@@ -15,7 +15,7 @@ namespace ÆGTESemesterProjekt.Services
             //_dbService = dbService;
             //_products = MockProducts.GetMockProducts();
             _products = JsonFileProductService.GetJsonObjects().ToList();
-            //JsonFileProductService.SaveJsonObjects(_products);
+            JsonFileProductService.SaveJsonObjects(_products);
             //_products = _dbService.GetProducts().Result;
         }
 
