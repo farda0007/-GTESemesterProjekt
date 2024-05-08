9cb23ef6e922c370408aee5215fa598f6037379f
Lucasloa <150661710+Lucasloa@users.noreply.github.com>
2024-05-08 09:27:11 +0200

    repair class added

--- ÆGTESemesterProjekt/Models/Repair.cs
+++ ÆGTESemesterProjekt/Models/Repair.cs
@@ -2,5 +2,19 @@
 {
 	public class Repair
 	{
-	}
+        public int CaseId { get; set; }
+        public DateTime SubDate { get; set; }
+        public string Description { get; set; }
+        public Product RepProduct { get; set; }
+        public string Image { get; set; }
+        public enum status 
+        {
+            Pending,
+            Finished, 
+            InProgress
+        }
+        public double RepPrice { get; set; }
+        public DateTime EstDate { get; set; }
+
+    }
 }
