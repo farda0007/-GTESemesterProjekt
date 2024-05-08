fb5501a097ad32328272d8a1fcd7c3097aa14fd6
Lucasloa <150661710+Lucasloa@users.noreply.github.com>
2024-05-08 10:26:37 +0200

    getallreparirs

--- ÆGTESemesterProjekt/Pages/RepairPage/GetAllRepairs.cshtml.cs
+++ ÆGTESemesterProjekt/Pages/RepairPage/GetAllRepairs.cshtml.cs
@@ -1,3 +1,4 @@
+using ÆGTESemesterProjekt.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,11 @@ namespace ÆGTESemesterProjekt.Pages.RepairPage
 {
     public class GetAllRepairsModel : PageModel
     {
+        private IRepairService _repairService;
+        public GetAllRepairsModel(IRepairService repairService) //Dependency Injection
+        {
+            this._repairService = repairService;
+        }
         public List<Models.Repair>? Repairs { get; private set; }
         public void OnGet()
         {
