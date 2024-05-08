a7e5c594b8389678ee2b474f45ed9204528f565b
Lucasloa <150661710+Lucasloa@users.noreply.github.com>
2024-05-08 09:48:30 +0200

    layout knap tilføjet

---
+++ ÆGTESemesterProjekt/Pages/RepairPage/GetAllRepairs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ÆGTESemesterProjekt.Pages.RepairPage
+{
+    public class GetAllRepairsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
