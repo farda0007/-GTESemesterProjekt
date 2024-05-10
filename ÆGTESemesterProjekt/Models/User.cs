2c1f1042b9df359e597764e948da2c0cb0f7ab5c
Lucasloa <150661710+Lucasloa@users.noreply.github.com>
2024-05-10 12:13:39 +0200

    NY restriction, USERS kan ikke hedde "employee" længere!

--- ÆGTESemesterProjekt/Models/User.cs
+++ ÆGTESemesterProjekt/Models/User.cs
@@ -13,8 +13,8 @@ namespace ÆGTESemesterProjekt.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
         public string UserName { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
+		public string Name { get; set; }
+		public string Password { get; set; }
         public int Phone { get; set; }
         public string Email { get; set; }
 		public virtual ICollection<Order> Orders { get; set; }
