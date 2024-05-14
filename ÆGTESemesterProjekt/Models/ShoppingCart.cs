b51ded17276bed8c7f1d450a2b04c689a122789f
farda0007 <ardaumutyildiz@gmail.com>
2024-05-14 16:27:28 +0200

    ShoppingCart

---
+++ ÆGTESemesterProjekt/Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿namespace ÆGTESemesterProjekt.Models
+{
+    public class ShoppingCart
+    {
+        public int UserId { get; set; }
+        public List<ShoppingCart> Products { get; set; }
+
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(int userId, List<ShoppingCart> products)
+        {
+            UserId = userId;
+            Products = products;
+        }
+    }
+}
---
+++ ÆGTESemesterProjekt/Services/ShoppingCartService.cs
@@ -0,0 +1,22 @@
+﻿using ÆGTESemesterProjekt.Models;
+
+namespace ÆGTESemesterProjekt.Services
+{
+    public class ShoppingCartService
+    {
+        private List<ShoppingCart> _cartProducts;
+        private DbGenericService<ShoppingCart> _dbService;
+
+        public ShoppingCartService(DbGenericService<ShoppingCart> dbService)
+        {
+            _dbService = dbService;
+            _cartProducts = _dbService.GetObjectsAsync().Result.ToList();
+        }
+
+        public void AddCardProduct(ShoppingCart cart)
+        {
+            _cartProducts.Add(cart);
+            _dbService.AddObjectAsync(cart);
+        }
+    }
+}
