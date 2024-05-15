078c3bc73c93356a01a04a1152612b702d3510a7
Lucasloa <150661710+Lucasloa@users.noreply.github.com>
2024-05-15 09:40:42 +0200

    placeholder for get all orders + orderservice getallorders metode

---
+++ ÆGTESemesterProjekt/Pages/Order/GetAllOrders.cshtml.cs
@@ -0,0 +1,26 @@
+using ÆGTESemesterProjekt.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ÆGTESemesterProjekt.Pages.Order
+{
+    public class GetAllOrdersModel : PageModel
+    {
+        private IProductService _productService;
+        public List<Models.Product>? Products { get; private set; }
+        public UserService UserService { get; set; }
+        public OrderService OrderService { get; set; }
+        public IEnumerable<Models.Order> MyOrders { get; set; }
+        public List<Models.Order>? Orders { get; private set; }
+
+        public GetAllOrdersModel(IProductService productService, OrderService orderService) //Dependency Injection
+        {
+            this._productService = productService;
+            orderService = OrderService;
+        }
+        public void OnGet()
+        {
+            Orders = OrderService.GetOrders();
+        }
+    }
+}
