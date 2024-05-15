1d495394316e3acd4a52b5af87e96d1814edfca1
Lucasloa <150661710+Lucasloa@users.noreply.github.com>
2024-05-15 09:21:12 +0200

    Productcatalog er færdig med alle typer objekter

--- ÆGTESemesterProjekt/Pages/ProductCatalog/CleaningTool.cshtml.cs
+++ ÆGTESemesterProjekt/Pages/ProductCatalog/CleaningTool.cshtml.cs
@@ -1,12 +1,28 @@
+using ÆGTESemesterProjekt.Models;
+using ÆGTESemesterProjekt.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using static ÆGTESemesterProjekt.Models.Product;
 
 namespace ÆGTESemesterProjekt.Pages.ProductCatalog
 {
     public class CleaningToolModel : PageModel
     {
-        public void OnGet()
+        private readonly IProductService _productService;
+
+        public CleaningToolModel(IProductService productService)
         {
+            _productService = productService;
+        }
+
+        public List<Product> CleaningToolProducts { get; private set; }
+
+        public IActionResult OnGet(string productType)
+        {
+            // Filter products based on the specified product type
+            CleaningToolProducts = _productService.GetProducts().Where(p => p.Type == Producttype.CleaningTool).ToList();
+
+            return Page();
         }
     }
 }
--- ÆGTESemesterProjekt/Pages/ProductCatalog/Headset.cshtml.cs
+++ ÆGTESemesterProjekt/Pages/ProductCatalog/Headset.cshtml.cs
@@ -1,12 +1,28 @@
+using ÆGTESemesterProjekt.Models;
+using ÆGTESemesterProjekt.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using static ÆGTESemesterProjekt.Models.Product;
 
 namespace ÆGTESemesterProjekt.Pages.ProductCatalog
 {
     public class HeadsetModel : PageModel
     {
-        public void OnGet()
+        private readonly IProductService _productService;
+
+        public HeadsetModel(IProductService productService)
         {
+            _productService = productService;
+        }
+
+        public List<Product> HeadsetProducts { get; private set; }
+
+        public IActionResult OnGet(string productType)
+        {
+            // Filter products based on the specified product type
+            HeadsetProducts = _productService.GetProducts().Where(p => p.Type == Producttype.Headset).ToList();
+
+            return Page();
         }
     }
 }
