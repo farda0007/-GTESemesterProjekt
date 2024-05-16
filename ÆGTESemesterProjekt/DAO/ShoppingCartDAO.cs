14fb3afd9844f858bbb44996349a492c167dc94c
farda0007 <ardaumutyildiz@gmail.com>
2024-05-16 13:19:31 +0200

    DAO udkommenteret

--- ÆGTESemesterProjekt/DAO/ShoppingCartDAO.cs
+++ ÆGTESemesterProjekt/DAO/ShoppingCartDAO.cs
@@ -5,11 +5,11 @@ namespace ÆGTESemesterProjekt.DAO
 {
     public class ShoppingCartDAO
     {
-        public int CartId { get; set; }
-        public int userId { get; set; }
-        public Product Product { get; set; }
-        public User User { get; set; }
-        public int Count { get; set; }
-        public int ProductId { get; set; }
+        //public int CartId { get; set; }
+        //public int userId { get; set; }
+        //public Product Product { get; set; }
+        //public User User { get; set; }
+        //public int Count { get; set; }
+        //public int ProductId { get; set; }
     }
 }
