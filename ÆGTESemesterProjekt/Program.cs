596986f8e24d27fbfd96b5a2ca8bfc1ac94e3545
farda0007 <ardaumutyildiz@gmail.com>
2024-05-17 11:23:53 +0200

    WIshlist fjernet

--- ÆGTESemesterProjekt/Program.cs
+++ ÆGTESemesterProjekt/Program.cs
@@ -19,7 +19,6 @@ builder.Services.AddSingleton<IProductService, ProductService>();
 builder.Services.AddSingleton<IRepairService, RepairService>();
 builder.Services.AddSingleton<OrderService, OrderService>();
 builder.Services.AddSingleton<ShoppingCartService, ShoppingCartService>();
-builder.Services.AddSingleton<WishlistService, WishlistService>();
 builder.Services.AddTransient<JsonFileService<Product>>();
 builder.Services.AddTransient<JsonFileService<User>>();
 builder.Services.AddTransient<JsonFileService<Employee>>();
