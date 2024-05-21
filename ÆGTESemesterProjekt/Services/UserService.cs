9687723eeb7323d093b646199fc8c5094a2e71fd
Lucasloa <150661710+Lucasloa@users.noreply.github.com>
2024-05-21 10:03:54 +0200

    users nu lavet

--- ÆGTESemesterProjekt/Services/UserService.cs
+++ ÆGTESemesterProjekt/Services/UserService.cs
@@ -16,11 +16,11 @@ namespace ÆGTESemesterProjekt.Services
         {
             _userJsonFileService = UserJsonFileService;
             _dbService = dbService;
-            Users = MockUsers.GetUsers();
+            //Users = MockUsers.GetUsers();
             //Users = _userJsonFileService.GetJsonObjects().ToList();
-            //Users = _dbService.GetObjectsAsync().Result.ToList();
+            Users = _dbService.GetObjectsAsync().Result.ToList();
             //_userJsonFileService.SaveJsonObjects(Users);
-            //_dbService.SaveObjects(Users);
+            _dbService.SaveObjects(Users);
             //LoggedInUser = Users[0];
         }
 
