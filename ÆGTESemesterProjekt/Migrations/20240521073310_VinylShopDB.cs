8927e468984edca95c2f8bd8243934d1553fad32
ThorRNES <thor@nesbit.dk>
2024-05-21 09:38:48 +0200

    db virker

---
+++ ÆGTESemesterProjekt/Migrations/20240521073310_VinylShopDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ÆGTESemesterProjekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class VinylShopDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
