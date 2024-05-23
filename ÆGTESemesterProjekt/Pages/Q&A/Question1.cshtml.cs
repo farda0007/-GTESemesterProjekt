a56918cffc13e07037b06e32671846be757d0c25
farda0007 <ardaumutyildiz@gmail.com>
2024-05-23 11:37:50 +0200

    1. spørgsmål

---
+++ ÆGTESemesterProjekt/Pages/Q&A/Question1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ÆGTESemesterProjekt.Pages.Q_A
+{
+    public class Question1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
