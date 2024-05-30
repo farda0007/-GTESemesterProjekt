429931f60954e4c355d7cba9cab9a610d892d8c5
Lasse Hammer <lasse@thomasminde.dk>
2024-05-31 00:34:57 +0200

    productpage update, sign up update, search fixed, css på index i stylesheet

--- ÆGTESemesterProjekt/Pages/Products/GetAllProducts.cshtml.cs
+++ ÆGTESemesterProjekt/Pages/Products/GetAllProducts.cshtml.cs
@@ -1,32 +1,37 @@
 using ÆGTESemesterProjekt.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Reflection;
 
 namespace ÆGTESemesterProjekt.Pages.Products
 {
     public class GetAllProductsModel : PageModel
     {
-		private IProductService _productService;
-		public List<Models.Product>? Products { get; private set; }
-		[BindProperty] 
-		public string SearchString { get; set; }
+        private IProductService _productService;
+        public List<Models.Product>? Products { get; private set; }
+        [BindProperty]
+        public string SearchString { get; set; }
         [BindProperty]
         public int MinPrice { get; set; }
         [BindProperty]
         public int MaxPrice { get; set; }
         public GetAllProductsModel(IProductService productService) //Dependency Injection
-		{
-			this._productService = productService;
-		}
-		public void OnGet()
-		{
-			Products = _productService.GetProducts();
-		}
-		public IActionResult OnPostNameSearch()
-		{
-			Products = _productService.NameSearch(SearchString).ToList();
-			return Page();
+        {
+            this._productService = productService;
+        }
+        public void OnGet()
+        {
+            Products = _productService.GetProducts();
+        }
+        public IActionResult OnPostNameSearch()
+        {
+            if (string.IsNullOrWhiteSpace(SearchString))
+            {
+
+                return RedirectToPage("GetAllProducts");
+            }
+
+            Products = _productService.NameSearch(SearchString).ToList();
+            return Page();
         }
 
         public IActionResult OnPostPriceFilter()
