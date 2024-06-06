59df371a5d8f53ae0f193020ea59013022ff4e24
ThorRNES <thor@nesbit.dk>
2024-06-06 10:39:30 +0200

    om os

---
+++ ÆGTESemesterProjekt/Pages/Inspiration.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ÆGTESemesterProjekt.Pages
+{
+    public class InspirationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
